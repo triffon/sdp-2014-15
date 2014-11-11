5e2471ab79c4f64c588d599edb7c935201054637
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2014-11-11 16:48:49 +0200

    Примерна програма за опашка.

---
+++ queue/queue_test.cpp
@@ -0,0 +1,36 @@
+/*
+ * queue_test.cpp
+ *
+ *  Created on: 11.11.2014 г.
+ *      Author: trifon
+ */
+
+#include <iostream>
+#include "squeue.cpp"
+#include "lstack.cpp"
+using namespace std;
+
+void simpleQueueTest() {
+	SQueue<int> q;
+	for(int i = 1; i <= 20; i++)
+		q.push(i);
+	while (!q.empty())
+		cout << q.pop() << ' ';
+	cout << endl;
+}
+
+void pushPopTest() {
+	SQueue<int> q;
+	for(int i = 1; i <= 1000; i++) {
+		q.push(i);
+		cout << q.pop() << endl;
+	}
+}
+
+int main() {
+	// simpleQueueTest();
+	pushPopTest();
+	return 0;
+}
+
+
