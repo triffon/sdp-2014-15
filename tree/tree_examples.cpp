b8fca5fafb3019d25fbf82e9087cfe7bade4ca35
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2015-01-05 16:45:00 +0200

    Реализация на извеждане на ниво в дърво.

--- tree/tree_examples.cpp
+++ tree/tree_examples.cpp
@@ -9,6 +9,17 @@
 
 typedef Tree<int> IntTree;
 
+template <typename T>
+void printLevel(Tree<T> const& t, int k) {
+	if (k < 1)
+		return;
+	if (k == 1)
+		cout << *t << ' ';
+	else
+		for(typename Tree<T>::I it = t.children(); it; ++it)
+			printLevel(*it, k - 1);
+}
+
 void testTree() {
 	IntTree t(0);
 	t.addChild(IntTree(1)
@@ -20,6 +31,9 @@ void testTree() {
 			.addChild(IntTree(32))
 			.addChild(IntTree(33)));
 	cout << t;
+	printLevel(t, 1);cout << endl;
+	printLevel(t, 2);cout << endl;
+	printLevel(t, 3);cout << endl;
 }
 
 int main() {
