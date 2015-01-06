0530eb897daa13ddc3f4b3d31b576ae3be2de0ce
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2015-01-06 16:30:00 +0200

    Примерни програми: дълбочина на дърво, дърво на аритметичен израз.

--- tree/tree_examples.cpp
+++ tree/tree_examples.cpp
@@ -7,6 +7,7 @@
 
 #include "tree.cpp"
 #include "bintree.cpp"
+#include "lstack.cpp"
 
 typedef Tree<int> IntTree;
 typedef BinaryTree<int> BIntTree;
@@ -38,6 +39,77 @@ void testTree() {
 	printLevel(t, 3);cout << endl;
 }
 
+template <typename T>
+int depth(BinaryTreeIterator<T> it) {
+	if (!it)
+		return 0;
+	return 1 + max(depth(++it), depth(it++));
+}
+
+int priority(char op) {
+	switch (op) {
+	case '+':
+	case '-':return 1;
+	case '*':
+	case '/':return 2;
+	default:return -1;
+	}
+}
+
+void createTree(LinkedStack<BinaryTree<char> >& rstack, char op) {
+	BinaryTree<char> rtree = rstack.pop();
+	BinaryTree<char> ltree = rstack.pop();
+	rstack.push(BinaryTree<char>(op, ltree, rtree));
+}
+
+BinaryTree<char> createExpressionTree(char const* expr) {
+	LinkedStack<char> opstack;
+	LinkedStack<BinaryTree<char> > rstack;
+
+	while(*expr) {
+		if (*expr >= '0' && *expr <= '9')
+			rstack.push(BinaryTree<char>(*expr));
+		else
+		if (*expr == '(')
+			opstack.push(*expr);
+		else
+		if (*expr == ')') {
+			char op = opstack.pop();
+			while (op != '(') {
+				createTree(rstack, op);
+				op = opstack.pop();
+			}
+		} else {
+			// *expr е операция
+			// първо: вадим всички операции
+			// с по-висок или равен приоритет
+			while (!opstack.empty() &&
+				   priority(opstack.last()) >=
+				   priority(*expr))
+				createTree(rstack,opstack.pop());
+			opstack.push(*expr);
+		}
+		expr++;
+	}
+	while (!opstack.empty())
+		createTree(rstack, opstack.pop());
+
+	return rstack.pop();
+}
+
+double calculate(BinaryTreeIterator<char> it) {
+	if (*it >= '0' && *it <= '9')
+		// листо
+		return *it - '0';
+	switch (*it) {
+	case '+': return calculate(++it) + calculate(it++);
+	case '-': return calculate(++it) - calculate(it++);
+	case '*': return calculate(++it) * calculate(it++);
+	case '/': return calculate(++it) / calculate(it++);
+	default: return 0;
+	}
+}
+
 void testBinaryTree() {
 	BIntTree empty;
 	BIntTree t4(4, empty, empty);
@@ -47,11 +119,23 @@ void testBinaryTree() {
 	BIntTree t5(5, empty, t6);
 	BIntTree t(1, t2, t5);
 	cout << t << endl;
+	cout << "Depth: " << depth(t.iterator()) << endl;
+}
+
+int const MAX = 100;
+
+void testExpressionTree() {
+	char expr[MAX];
+	cin.getline(expr, MAX);
+	BinaryTree<char> t = createExpressionTree(expr);
+	cout << t << endl;
+	cout << calculate(t.iterator()) << endl;
 }
 
 int main() {
 	// testTree();
-	testBinaryTree();
+	// testBinaryTree();
+	testExpressionTree();
 	return 0;
 }
 
